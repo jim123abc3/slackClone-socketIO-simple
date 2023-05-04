19e603d736ffccccbe012cc9d1fe554572375ad6
Jim Plimmer <jim123abc3@gmail.com>
2023-05-04 12:45:16 +0100

    implement namespace change, prevent duplicate sockets running

--- slackClone/public/scripts.js
+++ slackClone/public/scripts.js
@@ -3,6 +3,21 @@ const password = 'X'
 
 const socket = io('http://localhost:9000');
 
+const nameSpaceSockets = [];
+const listeners = {
+  nsChange: [],
+}
+
+const addListeners = (nsId) => {
+  if (!listeners.nsChange[nsId]) {
+    nameSpaceSockets[nsId].on('nsChange', (data) => {
+      console.log('Namespace changed');
+      console.log(data);
+    });
+    listeners.nsChange[nsId] = true;
+  }
+}
+
 socket.on('connect', () => {
   console.log('Conneceted!!');
   socket.emit('clientConnect');
@@ -10,10 +25,18 @@ socket.on('connect', () => {
 
 socket.on('nsList', (nsData) => {
   // const lastNs = localStorage.getItem('lastNs');
+  console.log(nsData);
+
   const nameSpacesDiv = document.querySelector('.namespaces');
   nameSpacesDiv.innerHTML = '';
   nsData.forEach(ns => {
-    nameSpacesDiv.innerHTML += `<div class="namespace" ns="${ns.endpoint}"><img src="${ns.image}"></div>`
+    nameSpacesDiv.innerHTML += `<div class="namespace" ns="${ns.endpoint}"><img src="${ns.image}"></div>`;
+
+    if (!nameSpaceSockets[ns.id]) {
+      nameSpaceSockets[ns.id] = io(`http://localhost:9000${ns.endpoint}`);
+    }
+    addListeners(ns.id);
+
   });
 
   Array.from(document.getElementsByClassName('namespace')).forEach(element => {
@@ -23,5 +46,5 @@ socket.on('nsList', (nsData) => {
   });
 
   joinNs(document.getElementsByClassName('namespace')[0], nsData);
-
+ 
 });
--- slackClone/slack.js
+++ slackClone/slack.js
@@ -3,18 +3,31 @@ const app = express();
 const socketio = require('socket.io')
 
 const namespaces = require('./data/namespaces')
+const Room = require('./classes/Room');
 
 app.use(express.static(__dirname + '/public'));
 
 const expressServer = app.listen(9000);
-const io = socketio(expressServer)
+const io = socketio(expressServer);
 
-io.on('connection', (socket) => {
-  socket.emit('welcome','Welcome to the server');
+app.get('/change-ns', (req,res) => {
+  namespaces[0].addRoom(new Room(0, 'Deleted Articles', 0));
+  io.of(namespaces[0].endpoint).emit('nsChange', namespaces[0]);
+
+  res.json(namespaces[0])
+});
+
+io.on('connection', (socket) => { 
+  socket.emit('welcome', 'Welcome to the server');
 
   socket.on('clientConnect', (data) => {
-    console.log(socket.id,'has connected')
+    console.log(socket.id, 'has connected')
+    socket.emit('nsList', namespaces)
   });
+})
 
-  socket.emit('nsList', namespaces)
+namespaces.forEach(namespace => {
+  io.of(namespace.endpoint).on('connection', (socket) => {
+    console.log(`${socket.id} has connected to ${namespace.endpoint}`);
+  })
 })
